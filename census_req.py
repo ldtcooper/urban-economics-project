73e9d8f1d8b33babaf61ef10e2ebe14af7743ca5
Logan Cooper <ldtcoop@gmail.com>
2023-04-10 14:29:17 -0400

    Switch to using 2019 ACS data

--- census_req.py
+++ census_req.py
@@ -22,7 +22,7 @@ def get_tract_data(c: Census, tract_vars: Tuple[str], year: int) -> None:
 API_KEY = load_api_key()
 
 # Define the variables to retrieve
-# get these here: https://api.census.gov/data/2020/acs/acs5/variables.html
+# get these here: https://api.census.gov/data/2019/acs/acs5/variables.html
 name = "NAME"
 mean_inc = "B19019_001E"
 num_public_transit = "B08301_010E"
@@ -30,17 +30,17 @@ population = "B01003_001E"
 building_const = [f'B25034_{"00" if i < 10 else "0"}{i}E' for i in range(2, 12)]
 
 msa_vars = (name, mean_inc)
-tract_vars_2020 = tuple([name, mean_inc, num_public_transit, population] + building_const)
+tract_vars_2019 = tuple([name, mean_inc, num_public_transit, population] + building_const)
 tract_vars_2000 = tuple([name] + building_const)
 
 c = Census(API_KEY)
 
 # handle MSA-level data
-msa_data = c.acs5.get(msa_vars, geo={"for": "metropolitan statistical area/micropolitan statistical area:*"}, year=2020)
+msa_data = c.acs5.get(msa_vars, geo={"for": "metropolitan statistical area/micropolitan statistical area:*"}, year=2019)
 
 with open('./data/msa_data.json', 'w') as f:
     json.dump(msa_data, f)
 
-get_tract_data(c, tract_vars_2020, 2020)
+get_tract_data(c, tract_vars_2019, 2019)
 # get_tract_data(c, tract_vars_2000, 2000)
 
